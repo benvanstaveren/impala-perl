49ae91a9f81dd04f58cbf688a6d53f0517c546ca
Ben van Staveren <ben@blockstackers.net>
2015-10-14 21:15:32 +0200

    Initial commit + Added thrift files from impala-ruby; edited for Perl namespace + Generated Impala::Protocol:: tree from thrift files + Fixed generated files for private package declarations (go go gadget CPAN indexer)

---
+++ lib/Impala.pm
@@ -0,0 +1,5 @@
+use strict;
+use warnings;
+package Impala;
+
+1;
---
+++ lib/Impala/Protocol.pm
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+package
+    Impala::Protocol;
+
+use Impala::Protocol::Impala::ImpalaService;
+use Mojo::Base 'Impala::Protocol::Impala::ImpalaServiceClient';
+
+1;
